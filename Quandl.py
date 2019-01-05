8a3b2a77b44c89f4484ffcb59517cae280b182d4
mrhili <mohamedaminemrhili@users.noreply.github.com>
2019-01-05 13:53:25 +0100

    show

--- Quandl.py
+++ Quandl.py
@@ -5,10 +5,16 @@ Created on Tue Jan  1 03:01:05 2019
 @author: hp
 """
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+
+import matplotlib.pyplot as plt
+
+from matplotlib import style
+
+style.use('ggplot')
 
 
 quandl.ApiConfig.api_key = 'C7_Lrb9EY6hMduTn-GAb'
@@ -32,15 +38,18 @@ df.fillna(-99999, inplace=True)
 forecast_out = math.ceil( 0.01 * len(  df   )    )
 
 df['label'] = df[ forecast_col ].shift( - forecast_out )
+
+
+X = np.array( df.drop(['label'], 1) )
+
+
+X = preprocessing.scale( X )
+X_lately = X[ - forecast_out:]
+X = X[: - forecast_out:]
 df.dropna(inplace=True)
 
 #print(df.tail())
-
-X = np.array( df.drop(['label'], 1) )
 y = np.array( df['label'] )
-
-X = preprocessing.scale( X )
-
 
 y = np.array( df['label'] )
 
@@ -55,4 +64,30 @@ clf.fit( X_train , y_train )
 
 accuracy = clf.score(X_test, y_test)
 
-print( accuracy )
+#print( accuracy )
+
+forecast_set = clf.predict( X_lately )
+
+
+#print( forecast_set, accuracy, forecast_out )
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len( df.columns )-1)] + [i]
+    
+#print( df.head() )
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend( loc = 4)
+plt.xlabel( 'Date')
+plt.ylabel( 'Price')
+plt.show()
